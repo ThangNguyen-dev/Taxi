bf72d31ab243c03b4d06b1c25585f771892e1d35
unknown <Thang24122k@gmail.com>
2021-01-01 18:25:43 +0700

    update done

--- modules/add.php
+++ modules/add.php
@@ -1,10 +1,21 @@
 <?php
 
-function insertDistance($min, $max, $conn)
+function insertDistance(int $min, int $max, PDO $conn)
 {
-    $conn->query('INSERT INTO `distance`( `Min`, `Max`) VALUES (' . $min . ', ' . $max . ')');
+    $insert = $conn->prepare('INSERT INTO `distance`( `Min`, `Max`) VALUES (:Min,:Max)');
+    $insert->execute(['Min' => $min, 'Max' => $max]);
+    header("Location: distance.php");
 }
-function insertWeight($weight, $conn)
+function insertWeight(int $weight, PDO $conn)
 {
-    $conn->query('INSERT INTO `weight`( `weight`) VALUES (' . $weight . ')');
+    $insert = $conn->prepare('INSERT INTO `weight`( `weight`) VALUES( :weight)');
+    $insert->execute(['weight' => $weight]);
+    header("Location: weight.php");
+}
+function insertCost(int $idWeight, int $idDistance, PDO $conn, $fee)
+{
+    //
+    $insert = $conn->prepare('INSERT INTO `cost`(`id_distance`, `id_weight`, `fee`) VALUES (:idDistance,:idWeight,:fee)');
+    $insert->execute(['id_distance' => $idDistance, 'id_weight' => $idWeight, 'fee' => $fee]);
+    header("Location: weight.php");
 }
